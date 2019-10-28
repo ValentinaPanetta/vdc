64241de838487fbabd7bfd68bea86bf09597ef46
Tanja Kunz <tk.kunz@gmx.at>
2019-10-28 09:26:50 +0100

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,34 @@
-<footer class="footer w-100 bg-white shadow row d-flex justify-content-around text-center">
-	<div class="col-4 p-3">
+<footer class="footer w-100 shadow row d-flex justify-content-around align-content-center text-center">
+	<div  class="col-12 p-3 d-flex d-flex text-center">
+		<div class="d-flex text-center">
+			<div class="px-3 social"><i class="fab fa-facebook-square">t</i></div>
+			<div class="px-3 social"><i class="fab fa-instagram"></i>t</div>
+			<div class="px-3 social"><i class="fab fa-twitter-square">t</i></div>
+			<div class="px-3 social"><i class="fab fa-youtube">t</i></div>
+			<div class="px-3 social"><i class="fab fa-linkedin">t</i></div>
+		</div>
+	</div>
+	<div class="p-3 d-flex text-center">
+		<div class="px-3">
+			<a href="{{ url('contact') }}" >Contact</a>
+		</div>
+		<div class="px-3">
+			<a href="{{ url('newsletter') }}" >Newsletter</a>
+		</div>
+		<div class="px-3">
+			<a href="{{ url('legal') }}" >Legal Details</a>
+		</div>
+		<div class="px-3">
+			<a href="{{ url('press') }}" >Press</a>
+		</div>
+		<div class="px-3">
+			<a href="{{ url('dataprotection') }}" >Data Protection</a>
+		</div>
+		<div class="px-3">
+			<a href="{{ url('terms') }}" >Terms and Condition</a>
+		</div>
+	</div>
+	{{-- <div class="col-10 p-3">
 		<div >
 			<div>
 				<a href="{{ url('contact') }}" >Contact</a>
@@ -31,5 +60,5 @@
 				<a href="{{ url('terms') }}" >Terms and Condition</a>
 			</div>
 		</div>
-	</div>
+	</div> --}}
 </footer>
